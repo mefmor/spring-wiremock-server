280c04ea2cd9122ff6cf8d9503b40b8f0f21e0a2
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-01-24 16:53:08 +0300

    Add dump integration tests

--- src/test/java/net/mefmor/demo/spring/wiremock/server/SpringWireMockServerApplicationTests.java
+++ src/test/java/net/mefmor/demo/spring/wiremock/server/SpringWireMockServerApplicationTests.java
@@ -1,13 +1,35 @@
 package net.mefmor.demo.spring.wiremock.server;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 class SpringWireMockServerApplicationTests {
+    private TestRestTemplate template = new TestRestTemplate();
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeEach
+    void resetDefaultParameters() {
+        template.postForLocation("http://localhost:8080/__admin/mappings/reset", "");
+    }
 
+    @Test
+    void byDefaultThereIsNoResponse() {
+        assertThat(template.getForEntity("http://localhost:8080/endpoint", String.class).getBody(),
+                equalTo("No response could be served as there are no stub mappings in this WireMock instance."));
+    }
+
+    @Test
+    void youCanSetYourOwnResponseToEndpoint() {
+        String request = "{ \"request\": { \"url\": \"/endpoint\", \"method\": \"GET\" }, \"response\": { \"status\": 200, \"body\": \"Here it is!\" }}";
+
+        template.postForLocation("http://localhost:8080/__admin/mappings/new", request);
+
+        assertThat(template.getForEntity("http://localhost:8080/endpoint", String.class).getBody(),
+                equalTo("Here it is!"));
+    }
 }
