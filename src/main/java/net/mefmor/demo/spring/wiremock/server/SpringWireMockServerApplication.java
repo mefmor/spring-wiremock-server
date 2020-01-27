fdb04664845deb7bd41a5f7e6347569279193bc8
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-01-27 17:52:25 +0300

    Use Spring Cloud Contracts instead WireMock Standalone

--- src/main/java/net/mefmor/demo/spring/wiremock/server/SpringWireMockServerApplication.java
+++ src/main/java/net/mefmor/demo/spring/wiremock/server/SpringWireMockServerApplication.java
@@ -2,8 +2,11 @@ package net.mefmor.demo.spring.wiremock.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.wiremock.WireMockConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WireMockConfiguration.class)
 public class SpringWireMockServerApplication {
 
     public static void main(String[] args) {
